4cfe1e9c61aa04e5d471b683e491c65f72774e14
asif <asmu22ise@cmrit.ac.in>
2025-06-17 02:25:29 +0530

    server.js updated for vercel

--- server/lib/db.js
+++ server/lib/db.js
@@ -6,7 +6,7 @@ export const connectDB = async () => {
     mongoose.connection.on("connected", () => {
       console.log("database connected successfully");
     });
-    await mongoose.connect(`${process.env.MONGODB_URI}`);
+    await mongoose.connect(`${process.env.MONGODB_URI}/chat-app`);
   } catch (error) {
     console.log(error);
   }
