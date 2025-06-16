d4696afc345a4028badbe9c6e5ac0f7de43b00fd
asif <asmu22ise@cmrit.ac.in>
2025-06-17 02:27:37 +0530

    server.js for vercel

--- server/server.js
+++ server/server.js
@@ -12,20 +12,21 @@ dotenv.config();
 const app = express();
 const server = http.createServer(app);
 
-//Initialize socket.io server
+// Initialize socket.io server
 export const io = new Server(server, {
   cors: { origin: "*" },
 });
 
-//Store online users
+// Store online users
 export const userSocketMap = {};
 
-//socket.io connection handler
+// socket.io connection handler
 io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
   console.log("User connected", userId);
   if (userId) userSocketMap[userId] = socket.id;
-  //Emit online users to all connected client
+
+  // Emit online users to all connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
@@ -34,20 +35,26 @@ io.on("connection", (socket) => {
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });
-// middleware
+
+// Middleware
 app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
-// test route
+// Routes
 app.use("/api/status", (req, res) => res.send("Server is live"));
 app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter);
 
-// connect to DB and start server
+// Connect to DB and start server
 const startServer = async () => {
   await connectDB();
-  const PORT = process.env.PORT || 5000;
-  server.listen(PORT, () => console.log("Server is running " + PORT));
+
+  if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => console.log("Server is running on port " + PORT));
+  }
 };
 
 startServer();
+// Export the server for testing purposes
+export default server;
